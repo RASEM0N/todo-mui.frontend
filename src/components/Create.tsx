b168697df539dc510bc1fd9f4de3803523414523
RASEM0N <70789810+RASEM0N@users.noreply.github.com>
2021-04-09 21:10:53 +0500

    icons

--- src/components/Create.tsx
+++ src/components/Create.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Button, Typography } from '@material-ui/core'
+import AcUnitOutlinedIcon from '@material-ui/icons/AcUnitOutlined'
 
 export const Create: React.FC = () => {
     return (
@@ -20,6 +21,14 @@ export const Create: React.FC = () => {
             >
                 Submit
             </Button>
+            {/*icons*/}
+            <br />
+            <AcUnitOutlinedIcon />
+            <AcUnitOutlinedIcon color={'secondary'} fontSize={'large'} />
+            <AcUnitOutlinedIcon color={'secondary'} fontSize={'small'} />
+            <AcUnitOutlinedIcon color={'action'} fontSize={'small'} />
+            <AcUnitOutlinedIcon color={'error'} fontSize={'small'} />
+            <AcUnitOutlinedIcon color={'disabled'} fontSize={'small'} />
         </div>
     )
 }
