a8c3d0b113efb783d3d04c403318875e2ca93034
RASEM0N <70789810+RASEM0N@users.noreply.github.com>
2021-04-09 20:30:58 +0500

    End module

--- src/components/Create.tsx
+++ src/components/Create.tsx
@@ -1,18 +1,16 @@
 import React from 'react'
 import { Typography } from '@material-ui/core'
 
-// https://material-ui.com/ru/components/typography/#typography
-
 export const Create: React.FC = () => {
     return (
         <div>
-            <Typography variant="h4" color="primary" align="center">
-                Create a new note
-            </Typography>
-            <Typography color="secondary" noWrap>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                Consequatur debitis delectus dolorem est, iure iusto molestias
-                qui velit voluptates voluptatibus?
+            <Typography
+                variant={'h6'}
+                gutterBottom
+                color={'textSecondary'}
+                component={'h2'}
+            >
+                Create a New Note
             </Typography>
         </div>
     )
