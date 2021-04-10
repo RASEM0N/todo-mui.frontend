8bdc5c3074997d191adb443baac19564fb592000
RASEM0N <70789810+RASEM0N@users.noreply.github.com>
2021-04-10 20:12:23 +0500

    Permanent Drawer

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,10 +1,21 @@
 import React from 'react'
-import { makeStyles } from '@material-ui/core'
+import { makeStyles, Drawer, Typography } from '@material-ui/core'
+
+// Drawer:              https://material-ui.com/ru/components/drawers/#drawer
 
 const useStyles = makeStyles({
     page: {
         background: '#f9f9f9',
         width: '100%',
+    },
+    drawer: {
+        width: 240,
+    },
+    drawerPaper: {
+        width: 240,
+    },
+    root: {
+        display: 'flex',
     },
 })
 
@@ -15,10 +26,22 @@ interface LayoutProps {
 const Layout: React.FC<LayoutProps> = ({ children }) => {
     const classes = useStyles()
     return (
-        <div>
+        <div className={classes.root}>
             {/*app bar*/}
 
             {/*side bar*/}
+            <Drawer
+                className={classes.drawer}
+                variant={'permanent'}
+                anchor={'left'}
+                classes={{
+                    paper: classes.drawerPaper,
+                }}
+            >
+                <div>
+                    <Typography variant={'h5'}>Notes</Typography>
+                </div>
+            </Drawer>
 
             <div className={classes.page}>{children}</div>
         </div>
