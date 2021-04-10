fb4e19662350199c8b07397fa2e76371a49342b6
RASEM0N <70789810+RASEM0N@users.noreply.github.com>
2021-04-10 14:47:18 +0500

    Grid System

--- src/components/Notes.tsx
+++ src/components/Notes.tsx
@@ -1,5 +1,9 @@
 import React, { useEffect, useState } from 'react'
+import Grid from '@material-ui/core/Grid'
+import Paper from '@material-ui/core/Paper'
 import useFetch from './hooks/useFetch'
+
+// Grid:            https://material-ui.com/ru/components/grid/#grid
 
 export interface Note {
     title: string
@@ -7,7 +11,6 @@ export interface Note {
     category: string
     id: number
 }
-
 export type DataNotes = Array<Note>
 
 export const Notes: React.FC = () => {
@@ -19,6 +22,21 @@ export const Notes: React.FC = () => {
 
     return (
         <div>
+            <Grid container>
+                <Grid md={3} sm={6} xs={12} item>
+                    <Paper>1</Paper>
+                </Grid>
+                <Grid md={3} sm={6} xs={12} item>
+                    <Paper>2</Paper>
+                </Grid>
+                <Grid md={3} sm={6} xs={12} item>
+                    <Paper>3</Paper>
+                </Grid>
+                <Grid md={3} sm={6} xs={12} item>
+                    <Paper>4</Paper>
+                </Grid>
+            </Grid>
+
             {!loading &&
                 response &&
                 response.map((item) => <p key={item.id}>{item.title}</p>)}
